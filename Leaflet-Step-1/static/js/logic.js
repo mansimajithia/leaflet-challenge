58db473649254f84d97b31b8008441c06ed46ed2
mansimajithia <mansi.majithia2@gmail.com>
2020-07-09 22:07:25 -0500

    final commit

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,84 +1,91 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+// Stor out API endpoint inside queryURL
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-// If map is already initialized:
-var container = L.DomUtil.get('map'); if(container != null){ container._leaflet_id = null; }
 // Create myMap w/out overlays
 var myMap = L.map("map", {
     center: [
-      37.09, -95.71
+        37.09, -95.71       
     ],
-  });
+    zoom: 5
+});
 
-// TileLayer
+// Tile Layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
-    accessToken: "pk.eyJ1IjoibWFuc2ltYWppdGhpYSIsImEiOiJja2J2Z2FhZWYwNXJ6MzFvYjd6bWZ5dGUwIn0.qMbXMOKsox-8NIfFraGrIg"
-  }).addTo(myMap);
+    accessToken: API_KEY
+}).addTo(myMap);
 
-// Perform GET request to the query URL
-d3.json(queryUrl, function(data){
+// Perform GET Request to the queryURL
+d3.json(queryURL, function (data) {
     console.log(data);
 
     var features = data.features
 
     // For loop to get each magnitude, place, time, coordinate
 
-    for (var i=0; i<features.length; i++){
+    for (var i = 0; i < features.length; i++) {
         var feature = features[i];
         var magnitude = feature.properties.mag;
         var place = feature.properties.place;
         var earthquakeTime = Date(feature.properties.time);
-        var coordinates = feature.geometry.coordinates
-        
+        var coordinates = feature.geometry.coordinates;
+
+        var color;
         if (magnitude > 5) {
             color = "red";
         }
-        else if (magnitude > 4){
+        else if (magnitude > 4) {
             color = "orange"
         }
-        else if (magnitude > 3){
+        else if (magnitude > 3) {
             color = "gold"
         }
-        else if (magnitude > 2){
+        else if (magnitude > 2) {
             color = "yellow"
         }
-        else if (magnitude > 1){
+        else if (magnitude > 1) {
             color = "yellowgreen"
         }
         else {
             color = "green"
         }
-
-        L.circle([coordinates[1], coordinates[0]],{
+        function getColor(d) {
+            return d > 5 ? '#EA2C2C' :
+                   d > 4 ? '#EA822C' :
+                   d > 3 ? '#EE9C00' :
+                   d > 2 ? '#D4EE00' :
+                   d > 1 ? '#98EE00':
+                            '#CCFF33';
+        }
+        L.circle([coordinates[1], coordinates[0]], {
             color: color,
             fillcolor: color,
             fillOpacity: 0.75,
             radius: magnitude * 1000
         }).bindPopup("<h3>" + place +
-        "</h3><hr><p>" + earthquakeTime + "</p><hr><p> magnitude: " + magnitude + "</p>").addTo(myMap);
+            "</h3><hr><p>" + earthquakeTime + "</p><hr><p> magnitude: " + magnitude + "</p>").addTo(myMap);
     }
     console.log(earthquakeTime)
 
-    // Adding legend
-    var legend = L.control({position: 'bottomright'});
+    var legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function (map) {
 
-    legend.onAdd = function(map) {
-        var div = L.DomUtil.create ('div', 'info legend'),
-        grades = ['5+', '4-5', '3-4', '2-3', '1-2', '0-1'],
-        colors = ['red', 'orange', 'gold', 'yellow', 'yellowgreen', 'green'],
-        labels = [];
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 1, 2, 3, 4, 5],
+            labels = [];
 
-        var legendInfo = "<h2 style=\"color:red\">Earthquake Magnitude</h2>" +
-        "<div class=\"labels\">" +
-        "</div>";
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
 
-        div.innerHTML = legendInfo;
         return div;
-    }
+    };
     legend.addTo(myMap);
 })
